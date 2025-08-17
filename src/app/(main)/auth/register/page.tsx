98fee91b645d8991c2c24508457537859af8b15e
Павел Кузнецов <gotrik.smith@gmail.com>
2025-08-17 23:17:47 +0000

    что еще можно улучшить?

--- src/app/(main)/auth/register/page.tsx
+++ src/app/(main)/auth/register/page.tsx
@@ -1,5 +1,11 @@
 
 import { RegisterPage } from '@/views/register';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Регистрация | ProDvor',
+    description: 'Создайте свой аккаунт на платформе ProDvor.',
+};
 
 export default function Register() {
   return <RegisterPage />;
