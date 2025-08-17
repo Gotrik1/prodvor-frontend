467402665490db6b06b6ab4963860f0e0070d720
Павел Кузнецов <gotrik.smith@gmail.com>
2025-08-17 20:05:25 +0000

    что еще можно улучшить?

--- src/views/admin/pages/roles/index.tsx
+++ src/views/admin/pages/roles/index.tsx
@@ -1,4 +1,3 @@
-
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/shared/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/shared/ui/table";
 import { User, ClipboardList, Gavel, Briefcase, Shield, Megaphone, Handshake, Star } from "lucide-react";
