cf6141e32edb40f3d639f24ed6c89870671bf9e3
Павел Кузнецов <gotrik.smith@gmail.com>
2025-08-22 10:45:11 +0000

    продолжай

---
+++ src/views/tournaments/manage/ui/tabs/index.tsx
@@ -0,0 +1,13 @@
+
+export * from './overview-tab';
+export * from './participants-tab';
+export * from './bracket-tab';
+export * from './schedule-tab';
+export * from './media-tab';
+export * from './promo-tab';
+export * from './staff-tab';
+export * from './sponsors-tab';
+export * from './announcements-tab';
+export * from './settings-tab';
+export * from './finances-tab';
+export * from './analytics-tab';
