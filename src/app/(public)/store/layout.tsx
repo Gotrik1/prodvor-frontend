1a3003c249049305dd55edcae7210af083aa3bc3
Павел Кузнецов <gotrik.smith@gmail.com>
2025-08-25 09:12:12 +0000

    в магазине так и нет сайдбара футкра и хедера

--- src/app/(public)/store/layout.tsx
+++ src/app/(public)/store/layout.tsx
@@ -1,14 +1,14 @@
 
+import MainLayout from '@/app/(main)/layout';
+
 export default function StoreLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-col min-h-screen bg-background text-foreground">
-        <main className="flex-1">
-            {children}
-        </main>
-    </div>
+    <MainLayout>
+        {children}
+    </MainLayout>
   )
 }
