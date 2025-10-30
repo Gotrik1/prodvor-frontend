39ceee3e34912aa56e00a7fdd6a32b82c22a305d
Павел Кузнецов <gotrik.smith@gmail.com>
2025-10-30 14:55:17 +0000

    слушай у нас же все виды спорта на каждого юзера есть в бд, что ты изобр

--- src/entities/user/lib.ts
+++ src/entities/user/lib.ts
@@ -1,17 +1,11 @@
-
 import type { User } from "@/mocks/users";
-import { allSports, teams } from "@/mocks";
+import { allSports } from "@/mocks";
 
 // Centralized function to get all disciplines for a user
 export const getUserDisciplines = (user: User): string[] => {
-    const personalDisciplines = user.disciplines
+    // The user.disciplines array now holds all sport IDs, both personal and from teams.
+    // This logic is now handled during mock data initialization.
+    return user.disciplines
         .map(id => allSports.find(s => s.id === id)?.name)
         .filter((name): name is string => !!name);
-    
-    const teamDisciplines = teams
-        .filter(team => team.members.includes(user.id))
-        .map(team => team.game);
-        
-    const allDisciplinesSet = new Set([...personalDisciplines, ...teamDisciplines]);
-    return Array.from(allDisciplinesSet);
 };
