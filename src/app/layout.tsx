85819643977189dec5f1a977fd6b841bee80abf0
Павел Кузнецов <gotrik.smith@gmail.com>
2025-10-30 08:58:38 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 
+'use client';
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/shared/ui/toaster";
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/shared/ui/theme-provider';
 import { ScaleProvider } from '@/shared/ui/scale-provider';
+import React, { useState, useEffect } from 'react';
 
 
-export const metadata: Metadata = {
-  title: 'ProDvor - Твоя игра. Твои правила. Твоя арена.',
-  description: 'ProDvor — это социальная платформа, объединяющая дворовый спорт и любительский киберспорт. Создавай команду, находи соперников, участвуй в турнирах и строй свою спортивную карьеру.',
-};
+// export const metadata: Metadata = {
+//   title: 'ProDvor - Твоя игра. Твои правила. Твоя арена.',
+//   description: 'ProDvor — это социальная платформа, объединяющая дворовый спорт и любительский киберспорт. Создавай команду, находи соперников, участвуй в турнирах и строй свою спортивную карьеру.',
+// };
 
 const inter = Inter({
   subsets: ['latin', 'cyrillic'],
@@ -23,7 +26,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
+  const [isClient, setIsClient] = useState(false)
+ 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
     <html lang="ru" className={`${inter.variable}`} suppressHydrationWarning>
       <head>
@@ -41,7 +49,7 @@ export default function RootLayout({
             disableTransitionOnChange
         >
             <ScaleProvider>
-              {children}
+              {isClient ? children : null}
             </ScaleProvider>
             <Toaster />
         </ThemeProvider>
