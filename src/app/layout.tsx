aa577098576a79e9bba546f5b28ce3869ce95d80
Павел Кузнецов <gotrik.smith@gmail.com>
2025-11-02 15:48:47 +0000

    начнем с этого: react-dom-client.development.js:25022 Download the React

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="ProDvor" />
-        <link rel="manifest" href="/manifest.webmanifest" />
+        
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </head>
       <body className="font-body antialiased">
