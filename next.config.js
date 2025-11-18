8d32b8c820aeebb816fa74fa18542eb582273df1
Prodvor AI Frontend <ai-bot@prodvor.local>
2025-11-18 09:54:25 +0000

    auto: next.config.js saved at 2025-11-18T09:54:25Z

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
     '176.106.247.145',
 
     // Точные хосты ваших Workstations (из логов)
-    '9000-firebase-prodvor-backendgit-1762801375121.cluster-3iz2yhcd4rhr2rsnuja2mv2i6k.cloudworkstations.dev',
+    '9000-firebase-prodvor-backendgit-1762840441881.cluster-tafiw3cv6fduct4hlcc5knh5fo.cloudworkstations.dev',
     '5000-firebase-prodvor-backendgit-1762840441881.cluster-tafiw3cv6fduct4hlcc5knh5fo.cloudworkstations.dev',
     '8080-firebase-prodvor-backendgit-1762840441881.cluster-tafiw3cv6fduct4hlcc5knh5fo.cloudworkstations.dev',
 
